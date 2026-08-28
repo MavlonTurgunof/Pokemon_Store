c17bbd6b24d35e832e626cf4d93a6737c9f5f2fb
MavlonTurgunof <mavlondev2023@gmail.com>
2026-08-28 19:10:18 +0500

    Architecture is created

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { PokemonList } from './features/pokemon-list/pokemon-list';
+import { PokemonDetail } from './features/pokemon-detail/pokemon-detail';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'pokemon',
+    component: PokemonList,
+  },
+  {
+    path: 'pokemon/:id',
+    component: PokemonDetail,
+  },
+  {
+    path: '',
+    redirectTo: 'pokemon',
+    pathMatch: 'full',
+  },
+];
